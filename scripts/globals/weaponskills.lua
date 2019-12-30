8b69f43d8d5807de68577e6b1edd15063dc0f6bc
nappypup <nappypup@gmail.com>
2019-12-30 14:13:19 -0600

    remove acc bonus and penalty from levels on ws

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -100,7 +100,7 @@ function calculateRawWSDmg(attacker, target, wsID, tp, action, wsParams, calcPar
 
     -- Calculate critrates
     local critRate = 0
-    
+
     if (wsParams.canCrit) then -- Work out critical hit ratios
         local nativecrit = 0
         critrate = fTP(tp,wsParams.crit100,wsParams.crit200,wsParams.crit300)
@@ -146,7 +146,7 @@ function calculateRawWSDmg(attacker, target, wsID, tp, action, wsParams, calcPar
     -- Have to calculate added bonus for SA/TA here; since it is done outside of the fTP multiplier
     if attacker:getMainJob() == dsp.job.THF then
         -- Add DEX/AGI bonus to first hit if THF main and valid Sneak/Trick Attack
-        if calcParams.sneakApplicable then 
+        if calcParams.sneakApplicable then
             finaldmg = finaldmg +
                         (attacker:getStat(dsp.mod.DEX) * (1 + attacker:getMod(dsp.mod.SNEAK_ATK_DEX)/100) * calcParams.pdif) *
                         ((100+(attacker:getMod(dsp.mod.AUGMENTS_SA)))/100)
@@ -196,7 +196,7 @@ function calculateRawWSDmg(attacker, target, wsID, tp, action, wsParams, calcPar
         finaldmg = finaldmg + souleaterBonus(attacker, (calcParams.tpHitsLanded+calcParams.extraHitsLanded))
     end
 
-    -- Factor in "all hits" bonus damage mods 
+    -- Factor in "all hits" bonus damage mods
     local bonusdmg = attacker:getMod(dsp.mod.ALL_WSDMG_ALL_HITS) -- For any WS
     if (attacker:getMod(dsp.mod.WEAPONSKILL_DAMAGE_BASE + wsID) > 0) then -- For specific WS
         bonusdmg = bonusdmg + attacker:getMod(dsp.mod.WEAPONSKILL_DAMAGE_BASE + wsID)
@@ -392,7 +392,7 @@ function doMagicWeaponskill(attacker, target, wsID, wsParams, tp, action, primar
 
         dmg = dmg * ftp
 
-        -- Factor in "all hits" bonus damage mods 
+        -- Factor in "all hits" bonus damage mods
         local bonusdmg = attacker:getMod(dsp.mod.ALL_WSDMG_ALL_HITS) -- For any WS
         if (attacker:getMod(dsp.mod.WEAPONSKILL_DAMAGE_BASE + wsID) > 0) then -- For specific WS
             bonusdmg = bonusdmg + attacker:getMod(dsp.mod.WEAPONSKILL_DAMAGE_BASE + wsID)
@@ -560,11 +560,11 @@ function getHitRate(attacker,target,capHitRate,bonus)
 
     acc = acc + bonus
 
-    if (attacker:getMainLvl() > target:getMainLvl()) then -- acc bonus!
-        acc = acc + ((attacker:getMainLvl()-target:getMainLvl())*4)
-    elseif (attacker:getMainLvl() < target:getMainLvl()) then -- acc penalty :(
-        acc = acc - ((target:getMainLvl()-attacker:getMainLvl())*4)
-    end
+    --if (attacker:getMainLvl() > target:getMainLvl()) then -- acc bonus!
+      --  acc = acc + ((attacker:getMainLvl()-target:getMainLvl())*4)
+    --elseif (attacker:getMainLvl() < target:getMainLvl()) then -- acc penalty :(
+        --acc = acc - ((target:getMainLvl()-attacker:getMainLvl())*4)
+    --end
 
     local hitdiff = 0
     local hitrate = 75
@@ -607,11 +607,11 @@ function getRangedHitRate(attacker,target,capHitRate,bonus)
 
     acc = acc + bonus
 
-    if (attacker:getMainLvl() > target:getMainLvl()) then -- acc bonus!
-        acc = acc + ((attacker:getMainLvl()-target:getMainLvl())*4)
-    elseif (attacker:getMainLvl() < target:getMainLvl()) then -- acc penalty :(
-        acc = acc - ((target:getMainLvl()-attacker:getMainLvl())*4)
-    end
+    --if (attacker:getMainLvl() > target:getMainLvl()) then -- acc bonus!
+        --acc = acc + ((attacker:getMainLvl()-target:getMainLvl())*4)
+    --elseif (attacker:getMainLvl() < target:getMainLvl()) then -- acc penalty :(
+        --acc = acc - ((target:getMainLvl()-attacker:getMainLvl())*4)
+    --end
 
     local hitdiff = 0
     local hitrate = 75
@@ -627,8 +627,8 @@ function getRangedHitRate(attacker,target,capHitRate,bonus)
 
     -- Applying hitrate caps
     if (capHitRate) then -- this isn't capped for when acc varies with tp, as more penalties are due
-        if (hitrate>0.95) then
-            hitrate = 0.95
+        if (hitrate>0.99) then
+            hitrate = 0.99
         end
         if (hitrate<0.2) then
             hitrate = 0.2
@@ -928,7 +928,7 @@ function getAlpha(level)
     elseif (level < 99) then
         alpha = 0.85
     else
-        alpha = 1; 
+        alpha = 1;
     end
     return alpha
 end
